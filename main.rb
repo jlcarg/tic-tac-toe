45b1ae442a2f8454095c6417411d8136f9eeb00c
Juan L C <202941136+jlcarg@users.noreply.github.com>
2025-05-31 15:29:28 +0900

    Refactor code to display board after each play

--- main.rb
+++ main.rb
@@ -8,8 +8,10 @@ player2 = Player.new('O', 'player2')
 
 player_turn = player1
 
+board.display_board
+
 until board.check_win
-  board.display_board
   player_turn.make_a_play(board)
   player_turn = player_turn == player1 ? player2 : player1
+  board.display_board
 end
