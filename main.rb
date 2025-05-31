8261ecb90afd3bb0f477a8648390af2c54271eb6
Juan L C <202941136+jlcarg@users.noreply.github.com>
2025-05-31 16:34:12 +0900

    Solve issue that caused message to be displayed 2x

--- main.rb
+++ main.rb
@@ -13,11 +13,11 @@ puts "\nTIC TAC TOE on the command line\n by jlcarg\n"
 board.display_board
 
 until board.check_win
-  player_turn.make_a_play(board)
-  player_turn = player_turn == player1 ? player2 : player1
-  board.display_board
-  if board.number_of_plays == 9 && board.check_win != true
+  if board.number_of_plays == 9
     puts "It's a DRAW"
     break
   end
+  player_turn.make_a_play(board)
+  player_turn = player_turn == player1 ? player2 : player1
+  board.display_board
 end
