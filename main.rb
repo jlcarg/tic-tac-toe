bb34ac1d080e4916b3a3f3a029c066351d88d580
Juan L C <202941136+jlcarg@users.noreply.github.com>
2025-05-31 16:27:50 +0900

    Add check for draw cases
    Unintentionally adds an issue of sending 2 times a congratulatory message

--- main.rb
+++ main.rb
@@ -8,7 +8,7 @@ player2 = Player.new('O', 'Player 2')
 
 player_turn = player1
 
-puts "\nTIC TAC TOE on the command line\nby jlcarg\n"
+puts "\nTIC TAC TOE on the command line\n by jlcarg\n"
 
 board.display_board
 
@@ -16,4 +16,8 @@ until board.check_win
   player_turn.make_a_play(board)
   player_turn = player_turn == player1 ? player2 : player1
   board.display_board
+  if board.number_of_plays == 9 && board.check_win != true
+    puts "It's a DRAW"
+    break
+  end
 end
